ad660b8b41db7b523a018dcab5e9ac070fb2678a
katakawa <katakawa@gmail.com>
2021-07-17 13:59:44 -0500

    Refactor

--- src/main/java/com/algos/twodarray/MergeIntervals.java
+++ src/main/java/com/algos/twodarray/MergeIntervals.java
@@ -12,24 +12,20 @@ public class MergeIntervals {
   }
 
   public int[][] merge(int[][] intervals) {
-    LinkedList<int[]> merged = new LinkedList<>();
     Arrays.sort(intervals, (int[] a, int[] b) -> {
       return Integer.compare(a[0], b[0]);
     });
+    LinkedList<int[]> mergedIntervals = new LinkedList<>();
 
     for (int[] interval : intervals) {
-      // if the list of merged intervals is empty or if the current
-      // interval does not overlap with the previous, simply append it.
-      if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-        merged.add(interval);
+      if (mergedIntervals.isEmpty() || mergedIntervals.getLast()[1] < interval[0]) {
+        mergedIntervals.add(interval);
       }
-      // otherwise, there is overlap, so we merge the current and previous
-      // intervals.
       else {
-        merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+        mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1], interval[1]);
       }
     }
 
-    return merged.toArray(new int[merged.size()][2]);
+    return mergedIntervals.toArray(new int[mergedIntervals.size()][2]);
   }
 }
